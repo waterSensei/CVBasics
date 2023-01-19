2b904801cfb7c1bb830907c2b5f70c1579a249b1
waterSensei <simon.ss.shen@gmail.com>
2023-01-19 23:32:28 +1100

    modified:   ImageEditor/basics.py

--- ImageEditor/basics.py
+++ ImageEditor/basics.py
@@ -3,6 +3,26 @@ import matplotlib.image as img
 import numpy as np
 
 mimg = img.imread('image/image.jpg')
-mimg = 255 - mimg
-plt.imshow(mimg)
+[row, column] = [len(mimg), len(mimg[0])]
+
+# Negative
+mimgNeg = 255 - mimg
+plt.subplot(3, 2, 1)
+plt.imshow(mimgNeg)
+
+# Flip (or use PIL)
+mingFlip = np.zeros((row, column, 3))
+for i in range(column):
+    mingFlip[:, (column-i)-1] = mimg[:, i]
+plt.subplot(3, 2, 2)
+plt.imshow(mingFlip/255)
+
+# Color Channl Swap
+mingCSwap = np.zeros((row, column, 3))
+mingCSwap[:, :, 0] = mimg[:, :, 2]
+mingCSwap[:, :, 1] = mimg[:, :, 1]
+mingCSwap[:, :, 2] = mimg[:, :, 0]
+plt.subplot(3, 2, 3)
+plt.imshow(mingCSwap/255)
+
 plt.show()
